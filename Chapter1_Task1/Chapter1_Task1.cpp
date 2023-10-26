7fc24b124f852852117cd32a196474864dc72536
nimkaaa <94477159+nimkaaa@users.noreply.github.com>
2023-10-27 00:05:22 +0200

    Add files via upload
    Learning on Ravesli

---
+++ Chapter1_Task1/Chapter1_Task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int readNumber();
+void writeAnswer(int result);
+
+int main()
+{
+    int first_number = readNumber();
+    int second_number = readNumber();
+
+    writeAnswer(first_number + second_number);
+
+    return 0;
+}
+
+int readNumber()
+{
+    std::cout << "Enter an integer: ";
+    int a = 0;
+    std::cin >> a;
+
+    return a;
+}
+
+void writeAnswer(int result)
+{
+    std::cout << "Result of sum: " << result;
+}
