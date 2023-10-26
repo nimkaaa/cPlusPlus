2aed2c02f0be8e76171d9e7528118d4a34e7cf3a
nimkaaa <94477159+nimkaaa@users.noreply.github.com>
2023-10-27 00:14:56 +0200

    Add files via upload

---
+++ Chapter2_Task4/Chapter2_Task4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include "constants.h"
+
+int get_height();
+double get_passed_height(int time);
+
+int main()
+{
+    double tower_height = get_height(); // получаем высоту башни
+    int time = 0;
+
+    std::cout << "At 0 seconds, the ball is at height: " << tower_height << " meters\n";
+
+    for (time = 1; time < 6; time++)
+    {
+        double remainig_height = tower_height - get_passed_height(time);
+        if (remainig_height <= 0)
+        {
+            std::cout << "At " << time << " seconds, the ball is on the ground\n";
+            return 0;
+        }
+        else
+        {
+            std::cout << "At " << time << " seconds, the ball is at height: "
+                << remainig_height << " meters\n";
+        }
+    }
+
+    return 0;
+}
+
+int get_height()
+{
+    std::cout << "Enter the initial height of the tower in meters: ";
+    int height;
+    std::cin >> height;
+
+    return height;
+}
+
+double get_passed_height(int time)
+{
+    double result = myConstants::gravity * time * time / 2;
+
+    return result;
+}
---
+++ Chapter2_Task4/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace myConstants
+{
+	const double gravity = 9.8;
+}
+
+#endif // !CONSTANTS_H
+
